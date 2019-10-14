f4f3cabde1b24aa89013a3e95d4e5f317eae3a6d
BK <rlaqudrnr810@naver.com>
2019-10-14 21:06:53 +0900

    test

---
+++ app/src/main/java/com/example/topikhelper/first.java
@@ -0,0 +1,26 @@
+package com.example.topikhelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class first extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button button_mypage = (Button) findViewById(R.id.mypage);
+        button_mypage.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), Mypage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
