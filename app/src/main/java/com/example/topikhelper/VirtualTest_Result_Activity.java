09bf54b35b73c75f328c34e24ad6fe4060b15556
BK <rlaqudrnr810@naver.com>
2019-11-03 19:53:14 +0900

    for HW

---
+++ app/src/main/java/com/example/topikhelper/VirtualTest_Result_Activity.java
@@ -0,0 +1,14 @@
+package com.example.topikhelper;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VirtualTest_Result_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_virtualtest_result);
+    }
+}
